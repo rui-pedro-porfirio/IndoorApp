272edd325bcfc3e3d41ec10f04637c14740ef97e
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-04-09 01:20:57 +0100

    added new screen with new input of fingerprints

--- app/src/main/java/android/example/findlocation/activities/FingerprintingActivity.java
+++ app/src/main/java/android/example/findlocation/activities/FingerprintingActivity.java
@@ -4,12 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.example.findlocation.R;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+Needs Offline and Online Phase buttons
+(Offline phase)Needs filling scrolling, start button and finish button
+(Online phase)Needs current data window and position detected
+ */
 public class FingerprintingActivity extends AppCompatActivity {
+
+    private List<String> dataTypes;
+    private Map<String,Integer> preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fingerprinting);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        dataTypes = getIntent().getStringArrayListExtra("dataTypes");
+        preferences = (HashMap<String,Integer>) getIntent().getSerializableExtra("Preferences");
+        fillDataTypeTextView();
+        fillPreferencesTextView();
+    }
+
+    private void fillDataTypeTextView(){
+        TextView mDataTypeTextView = findViewById(R.id.dataTypesTextViewId);
+        String typeToAdd = "*";
+        for(int i = 0; i < dataTypes.size(); i++){
+            typeToAdd += dataTypes.get(i);
+            if(i != dataTypes.size() -1){
+                typeToAdd += "|";
+            }
+        }
+        mDataTypeTextView.setText(typeToAdd);
+    }
+
+    private void fillPreferencesTextView(){
+        TextView mPreferencesTextView = findViewById(R.id.preferencesId);
+        String preferencesToAdd = "*";
+        for(String key: preferences.keySet()){
+            preferencesToAdd += key + ":" + preferences.get(key) + "|";
+        }
+        mPreferencesTextView.setText(preferencesToAdd);
     }
 }
--- app/src/main/java/android/example/findlocation/activities/FingerprintingCheckboxActivity.java
+++ app/src/main/java/android/example/findlocation/activities/FingerprintingCheckboxActivity.java
@@ -2,20 +2,34 @@ package android.example.findlocation.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.example.findlocation.R;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FingerprintingCheckboxActivity extends AppCompatActivity {
+
+    private List<String> dataTypes;
+    private Map<String,Integer> preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //return button on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        dataTypes = new ArrayList<String>();
+        preferences = new HashMap<String,Integer>();
+        sendDataToActivity();
     }
 
     public void onCheckboxClicked(View view) {
@@ -28,18 +42,21 @@ public class FingerprintingCheckboxActivity extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.checkbox_wifi:
                 if(checked) {
+                    dataTypes.add("Wi-fi");
                     Toast.makeText(this, "Wi-Fi selected", Toast.LENGTH_SHORT).show();
                     mNextButton.setVisibility(View.VISIBLE);
                 }
                 break;
             case R.id.checkbox_bluetooth:
                 if(checked) {
+                    dataTypes.add("Bluetooth");
                     Toast.makeText(this, "Bluetooth selected", Toast.LENGTH_SHORT).show();
                     mNextButton.setVisibility(View.VISIBLE);
                 }
                 break;
             case R.id.checkbox_device_sensors:
                 if(checked) {
+                    dataTypes.add("DeviceData");
                     Toast.makeText(this, "Device Data selected", Toast.LENGTH_SHORT).show();
                     mNextButton.setVisibility(View.VISIBLE);
                 }
@@ -47,5 +64,24 @@ public class FingerprintingCheckboxActivity extends AppCompatActivity {
             default:
                 Toast.makeText(this,"Error in the selection",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void sendDataToActivity(){
+
+        final Intent mFingerprintScreenIntent = new Intent(this, FingerprintingActivity.class);
+        Button mNextButton = findViewById(R.id.nextId);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFingerprintScreenIntent.putStringArrayListExtra("dataTypes", (ArrayList<String>) dataTypes);
+
+                EditText mNumberOfFingerprints = findViewById(R.id.fingerprintPerLocationNumberId);
+                preferences.put("Number of Fingerprints",Integer.valueOf(mNumberOfFingerprints.getText().toString()));
+                EditText mTimeBetweenFingerprints = findViewById(R.id.intervalBetweenFingerprintsNumberId);
+                preferences.put("Time between Fingerprints",Integer.valueOf(mTimeBetweenFingerprints.getText().toString()));
+                mFingerprintScreenIntent.putExtra("Preferences",(Serializable) preferences);
+                startActivity(mFingerprintScreenIntent);
+            }
+        });
     }
 }
