dcc8091f98e28fb99c6ea75141550359f50164bd
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-04-27 21:07:14 +0100

    change django

--- Server/indoorAppServer/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Fingerprint,DeviceSensor,WiFiSensor,BluetoothSensor,language
-
-admin.site.register(Fingerprint)
-admin.site.register(DeviceSensor)
-admin.site.register(WiFiSensor)
-admin.site.register(BluetoothSensor)
-admin.site.register(language)
-
