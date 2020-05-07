6192b7dc9255072b31cbd9e9524376655930b986
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-05-07 14:15:40 +0100

    added mean filter handler

--- Server/indoorAppServer/views.py
+++ Server/indoorAppServer/views.py
@@ -43,11 +43,73 @@ class TypeEnum(Enum):
     BLUETOOTH = 2
 
 
+def apply_median_filter(fingerprints_reference_point, type, existing_fingerprint):  # AP1,AP2,AP3,AP1,AP4
+    if type == TypeEnum.WIFI:
+        access_points = WiFiSensor.objects.all().filter(fingerprint_id__in=fingerprints_reference_point.values('id')).values('name').annotate(
+            Count('name'))  # F1: AP1, AP2, AP3
+        for ap in access_points:  # AP1
+            similar_access_points = WiFiSensor.objects.filter(name=ap['name']).order_by('rssi')
+            rssi_values = list()
+            mac_address = ''
+            for sap in similar_access_points:
+                rssi_values.append(sap.rssi)
+                mac_address = sap.name
+            rssi_value_median = statistics.median(rssi_values)
+            similar_access_points.delete()
+            WiFiSensor.objects.create(name=mac_address,rssi=rssi_value_median,fingerprint_id=existing_fingerprint[0].id)
+    elif type == TypeEnum.BLUETOOTH:
+        beacons = BluetoothSensor.objects.all().filter(
+            fingerprint_id__in=fingerprints_reference_point.values('id')).values('name').annotate(
+            Count('name'))  # F1: AP1, AP2, AP3
+        for beacon in beacons:  # AP1
+            similar_beacons = BluetoothSensor.objects.filter(name=beacon['name']).order_by('rssi')
+            rssi_values = list()
+            mac_address = ''
+            for similar_beacon in similar_beacons:
+                rssi_values.append(similar_beacon.rssi)
+                mac_address = similar_beacon.name
+            rssi_value_median = statistics.median(rssi_values)
+            similar_beacons.delete()
+            BluetoothSensor.objects.create(name=mac_address, rssi=rssi_value_median,
+                                      fingerprint_id=existing_fingerprint[0].id)
+
+
+def apply_mean_filter(fingerprints_reference_point, type, existing_fingerprint):  # AP1,AP2,AP3,AP1,AP4
+    if type == TypeEnum.WIFI:
+        access_points = WiFiSensor.objects.all().filter(fingerprint_id__in=fingerprints_reference_point.values('id')).values('name').annotate(
+            Count('name'))  # F1: AP1, AP2, AP3
+        for ap in access_points:  # AP1
+            similar_access_points = WiFiSensor.objects.filter(name=ap['name']).order_by('rssi')
+            rssi_values = list()
+            mac_address = ''
+            for sap in similar_access_points:
+                rssi_values.append(sap.rssi)
+                mac_address = sap.name
+            rssi_value_median = statistics.median(rssi_values)
+            similar_access_points.delete()
+            WiFiSensor.objects.create(name=mac_address,rssi=rssi_value_median,fingerprint_id=existing_fingerprint[0].id)
+    elif type == TypeEnum.BLUETOOTH:
+        beacons = BluetoothSensor.objects.all().filter(
+            fingerprint_id__in=fingerprints_reference_point.values('id')).values('name').annotate(
+            Count('name'))  # F1: AP1, AP2, AP3
+        for beacon in beacons:  # AP1
+            similar_beacons = BluetoothSensor.objects.filter(name=beacon['name']).order_by('rssi')
+            rssi_values = list()
+            mac_address = ''
+            for similar_beacon in similar_beacons:
+                rssi_values.append(similar_beacon.rssi)
+                mac_address = similar_beacon.name
+            rssi_value_median = statistics.mean(rssi_values)
+            similar_beacons.delete()
+            BluetoothSensor.objects.create(name=mac_address, rssi=rssi_value_median,
+                                      fingerprint_id=existing_fingerprint[0].id)
+
+
 class FilterView(APIView):
 
     def get(self, request, format=None):
 
-        self.apply_filter(FilterEnum.MEDIAN_FILTER, 2)
+        self.apply_filter(FilterEnum.MEAN_FILTER, 2)
 
         return Response(status=status.HTTP_200_OK)
 
@@ -62,50 +124,9 @@ class FilterView(APIView):
             if number_partitions != number_fingerprints_per_reference_point:
                 if filter_identifier == FilterEnum.MEDIAN_FILTER:
                     existing_fingerprint = fingerprints_per_reference_point.all()[:1]
-                    self.apply_median_filter(fingerprints_per_reference_point, TypeEnum.WIFI,existing_fingerprint)
-                    self.apply_median_filter(fingerprints_per_reference_point, TypeEnum.BLUETOOTH,existing_fingerprint)
+                    apply_median_filter(fingerprints_per_reference_point, TypeEnum.WIFI,existing_fingerprint)
+                    apply_median_filter(fingerprints_per_reference_point, TypeEnum.BLUETOOTH,existing_fingerprint)
                 elif filter_identifier == FilterEnum.MEAN_FILTER:
-                    self.apply_mean_filter(wifi_raw_data_list)
-                    self.apply_mean_filter(bluetooth_raw_data_list)
-
-    def apply_median_filter(self, fingerprints_reference_point, type,existing_fingerprint):  # AP1,AP2,AP3,AP1,AP4
-        if type == TypeEnum.WIFI:
-            access_points = WiFiSensor.objects.all().filter(fingerprint_id__in=fingerprints_reference_point.values('id')).values('name').annotate(
-                Count('name'))  # F1: AP1, AP2, AP3
-            for ap in access_points:  # AP1
-                similar_access_points = WiFiSensor.objects.filter(name=ap['name']).order_by('rssi')
-                rssi_values = list()
-                mac_address = ''
-                for sap in similar_access_points:
-                    rssi_values.append(sap.rssi)
-                    mac_address = sap.name
-                rssi_value_median = statistics.median(rssi_values)
-                similar_access_points.delete()
-                WiFiSensor.objects.create(name=mac_address,rssi=rssi_value_median,fingerprint_id=existing_fingerprint[0].id)
-        elif type == TypeEnum.BLUETOOTH:
-            beacons = BluetoothSensor.objects.all().filter(
-                fingerprint_id__in=fingerprints_reference_point.values('id')).values('name').annotate(
-                Count('name'))  # F1: AP1, AP2, AP3
-            for beacon in beacons:  # AP1
-                similar_beacons = BluetoothSensor.objects.filter(name=beacon['name']).order_by('rssi')
-                rssi_values = list()
-                mac_address = ''
-                for similar_beacon in similar_beacons:
-                    rssi_values.append(similar_beacon.rssi)
-                    mac_address = similar_beacon.name
-                rssi_value_median = statistics.median(rssi_values)
-                similar_beacons.delete()
-                BluetoothSensor.objects.create(name=mac_address, rssi=rssi_value_median,
-                                          fingerprint_id=existing_fingerprint[0].id)
-
-
-    def apply_mean_filter(self, data):
-        rssi_values = list()
-        for item in data:
-            rssi_values.append(item.rssi)
-        print('PREVIOUS RSSI VALUES: ' + str(rssi_values))
-        print('UPDATED RSSI VALUES: ' + str(rssi_values))
-        length_data = len(data)
-        for i in range(length_data):
-            data[i].rssi = rssi_values[i]
-            data[i].save()
+                    existing_fingerprint = fingerprints_per_reference_point.all()[:1]
+                    apply_mean_filter(fingerprints_per_reference_point, TypeEnum.WIFI,existing_fingerprint)
+                    apply_mean_filter(fingerprints_per_reference_point, TypeEnum.BLUETOOTH,existing_fingerprint)
