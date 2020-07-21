93f31f42ba6a63a39bc8d563a01f4ba1471b2631
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-07-21 22:15:18 +0100

    updated trilat notebook

--- Server/indoorAppServer/snippets/distance.py
+++ Server/indoorAppServer/snippets/distance.py
@@ -51,8 +51,8 @@ room_limit_y_max = 4.0
 access_points = load_access_points_locations()
 display(access_points)
 distances_list = list()
-for i in np.arange(room_limit_x_min,room_limit_x_max,0.5):
-    for j in np.arange(room_limit_y_min,room_limit_y_max,0.5):
+for i in np.arange(room_limit_x_min,room_limit_x_max,1.0):
+    for j in np.arange(room_limit_y_min,room_limit_y_max,1.0):
         distances = {}
         distances[(i,j)] = compute_trilateration(x=i,y=j,access_points=access_points)
         distances_list.append(distances)
