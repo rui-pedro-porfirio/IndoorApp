ef058ae0b7531837e03cf7db749a32457f2a79e4
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-07-28 01:22:38 +0100

    added minimize func to trilateration notebook

--- Server/indoorAppServer/snippets/distance.py
+++ Server/indoorAppServer/snippets/distance.py
@@ -51,8 +51,8 @@ room_limit_y_max = 4.0
 access_points = load_access_points_locations()
 display(access_points)
 distances_list = list()
-for i in np.arange(room_limit_x_min,room_limit_x_max,0.1):
-    for j in np.arange(room_limit_y_min,room_limit_y_max,0.1):
+for i in np.arange(room_limit_x_min,room_limit_x_max,1):
+    for j in np.arange(room_limit_y_min,room_limit_y_max,1):
         distances = {}
         distances[(i,j)] = compute_trilateration(x=i,y=j,access_points=access_points)
         distances_list.append(distances)
@@ -75,4 +75,4 @@ for d in distances_list:
 display(dataframe_list)
 dataframe = pd.DataFrame(data=dataframe_list,columns=csv_columns)
 display(dataframe)
-dataframe.to_csv(r'.\trilateration_test_0.10_meters.csv', index = False)
+dataframe.to_csv(r'.\trilateration_test_v2.csv', index = False)
