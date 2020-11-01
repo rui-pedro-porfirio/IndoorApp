257f11fbdf3cd4618401dbb98752e8560a089f08
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-11-01 17:36:34 +0000

    Structure Commit. Removed Server folder from repo

--- Server/indoorAppServer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Fingerprint,DeviceSensor,WiFiSensor,BluetoothSensor
-
-admin.site.register(Fingerprint)
-admin.site.register(DeviceSensor)
-admin.site.register(WiFiSensor)
-admin.site.register(BluetoothSensor)
-
--- Server/indoorAppServer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IndoorAppServerConfig(AppConfig):
-    name = 'indoorAppServer'
